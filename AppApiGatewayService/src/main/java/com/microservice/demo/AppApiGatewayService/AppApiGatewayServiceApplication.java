64efc236b8a5e9f7285450df048c835c4a37055a
rage875 <rage.875@gmail.com>
2021-03-22 09:23:15 -0600

    AppApiGatewayService added

---
+++ AppApiGatewayService/src/main/java/com/microservice/demo/AppApiGatewayService/AppApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppApiGatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ AppApiGatewayService/src/test/java/com/microservice/demo/AppApiGatewayService/AppApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppApiGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
