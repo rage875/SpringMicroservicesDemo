fa9ea9d3bfe8dd6b6dc75c69265ab34e6aeed5e0
rage875 <rage.875@gmail.com>
2021-03-21 21:43:39 -0600

    AppDiscoveryService added

---
+++ AppDiscoveryService/src/main/java/com/microservice/demo/AppDiscoveryService/AppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ AppDiscoveryService/src/test/java/com/microservice/demo/AppDiscoveryService/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
