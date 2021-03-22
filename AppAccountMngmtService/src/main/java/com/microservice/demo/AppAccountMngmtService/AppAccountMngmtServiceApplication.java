583124b0b9564809eb74b211472a116b74a216d2
rage875 <rage.875@gmail.com>
2021-03-22 08:55:04 -0600

    AppAccountMngmtService added

---
+++ AppAccountMngmtService/src/main/java/com/microservice/demo/AppAccountMngmtService/AppAccountMngmtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppAccountMngmtService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAccountMngmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAccountMngmtServiceApplication.class, args);
+	}
+
+}
---
+++ AppAccountMngmtService/src/test/java/com/microservice/demo/AppAccountMngmtService/AppAccountMngmtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppAccountMngmtService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAccountMngmtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
