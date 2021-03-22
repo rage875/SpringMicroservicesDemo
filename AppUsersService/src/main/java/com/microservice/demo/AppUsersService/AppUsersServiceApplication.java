725b7bed02d38a4772ced4136a77ca3b2ee37468
rage875 <rage.875@gmail.com>
2021-03-21 21:20:07 -0600

    first commit: AppUsersService Spring project added

---
+++ AppUsersService/src/main/java/com/microservice/demo/AppUsersService/AppUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppUsersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUsersServiceApplication.class, args);
+	}
+
+}
