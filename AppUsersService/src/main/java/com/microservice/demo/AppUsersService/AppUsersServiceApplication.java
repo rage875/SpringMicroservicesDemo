739ec63eb50af3c2ee1b5bc59c624e841b751aee
rage875 <rage.875@gmail.com>
2021-03-23 17:13:25 -0600

    AppUsersService: Encrypt user password

--- AppUsersService/src/main/java/com/microservice/demo/AppUsersService/AppUsersServiceApplication.java
+++ AppUsersService/src/main/java/com/microservice/demo/AppUsersService/AppUsersServiceApplication.java
@@ -3,6 +3,8 @@ package com.microservice.demo.AppUsersService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,9 @@ public class AppUsersServiceApplication {
 		SpringApplication.run(AppUsersServiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPassowrdEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+	
 }
--- AppUsersService/src/main/java/com/microservice/demo/AppUsersService/service/UsersServiceImp.java
+++ AppUsersService/src/main/java/com/microservice/demo/AppUsersService/service/UsersServiceImp.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.microservice.demo.AppUsersService.data.UserEntity;
@@ -15,19 +16,22 @@ import com.microservice.demo.AppUsersService.shared.UserDto;
 public class UsersServiceImp implements UsersService {
 
 	UsersRepository usersRepository;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
-	public UsersServiceImp(UsersRepository usersRepository) {
+	public UsersServiceImp(UsersRepository usersRepository,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 
 		usersRepository.save(userEntity);
 
