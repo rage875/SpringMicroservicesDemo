e98e182407b649c23370979ba24224b9cb49cab7
rage875 <rage.875@gmail.com>
2021-03-23 17:36:31 -0600

    AppUsersService: Gateway web security

--- AppUsersService/src/main/java/com/microservice/demo/AppUsersService/security/WebSecurity.java
+++ AppUsersService/src/main/java/com/microservice/demo/AppUsersService/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.microservice.demo.AppUsersService.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,9 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
+	private Environment environment;
+	
+	@Autowired
+	public WebSecurity(Environment environment) {
+		this.environment = environment;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		//http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
 		http.authorizeRequests().antMatchers("/users/**").permitAll();
 		http.headers().frameOptions().disable();
 	}
