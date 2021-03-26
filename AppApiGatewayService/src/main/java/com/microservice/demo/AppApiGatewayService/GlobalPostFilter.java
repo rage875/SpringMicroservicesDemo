ac0f8004356ea95f428695a6d045994798378d22
rage875 <rage.875@gmail.com>
2021-03-25 21:12:44 -0600

    AppApiGatewayService: Global post filter implementation

---
+++ AppApiGatewayService/src/main/java/com/microservice/demo/AppApiGatewayService/GlobalPostFilter.java
@@ -0,0 +1,34 @@
+package com.microservice.demo.AppApiGatewayService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalPostFilter implements GlobalFilter, Ordered {
+
+	final Logger logger = LoggerFactory.getLogger(GlobalPostFilter.class);
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+		return chain.filter(exchange).then(Mono.fromRunnable(()-> {
+
+			logger.info("Global post filter executed...");
+
+		}));
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
