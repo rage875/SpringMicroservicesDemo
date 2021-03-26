edeede7cc98e140d6cef586468f7a9c80c7b907a
rage875 <rage.875@gmail.com>
2021-03-26 09:14:15 -0600

    AppApiConfigServer project added

---
+++ AppApiConfigServer/src/main/java/com/microservice/demo/AppApiConfigServer/AppApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppApiConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ AppApiConfigServer/src/test/java/com/microservice/demo/AppApiConfigServer/AppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.AppApiConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
